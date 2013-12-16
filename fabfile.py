42bd8d4a70f2d48fe63f27d4f1b8980ce921ac48
Oladayo Osikoya <alwaysdeone@gmail.com>
2013-12-16 16:39:17 +0100

    Add test feature to fab deploy.

--- fabfile.py
+++ fabfile.py
@@ -7,36 +7,41 @@ env.roledefs = {
 }
 env.shell = '/bin/bash -c'
 
+test_recipients="osikoya.oladayo@tecnotree.com"
+test_cc="alwaysdeone@gmail.com"
+
+live_recipients="rjmalm@mtn.com.gh;dtenartey@mtn.com.gh;soakoto@mtn.com.gh;msali@mtn.com.gh;titani@mtn.com.gh;jkbam@mtn.com.gh;abfaisal@mtn.com.gh;dannan@mtn.com.gh;sannan@mtn.com.gh;doseiboateng@mtn.com.gh"
+live_cc="jegadeesan.velusamy@tecnotree.com;osikoya.oladayo@tecnotree.com;adetimilehin.hammed@tecnotree.com;Chandra.Mohan@tecnotree.com"
+
 @hosts('pm_client@10.139.41.18')
 def deploy():
-    # Local
-    # =====
+    # 1. Archive app.
     local("mkdir -p fabrice/reports")
-    local("cp reports/data_offer_rental.sh reports/reports.cfg.sh fabrice/reports/")
+    local("cp reports/data_offer_rental.sh reports/invoice.sh reports/reports.cfg.sh fabrice/reports/")
 
     with lcd("fabrice/reports"):
-	local("mkdir include out logs")
-
-    local("cp reports/include/queries.sh fabrice/reports/include/")
+	local("mkdir out logs")
 
     with lcd("fabrice/reports/out"):
 	local("mkdir emails files results")
 
     local("zip -r fabrice fabrice")
 
-    # Remote
-    # ======
-    # Copy app archive to host, unzip and delete archive
     try:
+	# 2. Copy archive over to host.
 	put("fabrice.zip", "fabrice.zip")
+
+	# 3. Unzip archive/Deploy app.
+	run("rm -r fabrice")
 	run("unzip fabrice.zip")
 	run("rm fabrice.zip")
 
-	# Backup crontab with the exception of the job containing the script name
-	run("crontab -l | grep -v 'data_offer_rental.sh' > tmpcrontab")
+	# Backup crontab with the exception of the jobs to be deployed
+	run("crontab -l | grep -v 'data_offer_rental.sh' | grep -v 'invoice.sh' > tmpcrontab")
 
 	# Append script's cron job to the crontab backup
 	run("echo '00 12 02 * * sh fabrice/reports/data_offer_rental.sh >> fabrice/reports/logs/data_offer_rental.log 2>&1' >> tmpcrontab")
+	run("echo '00 12 07 * * sh fabrice/reports/invoice.sh >> fabrice/reports/logs/invoice.log 2>&1' >> tmpcrontab")
 
 	# Replace current crontab with backup
 	run("crontab -r")
@@ -44,9 +49,25 @@ def deploy():
 
 	# Delete backup
 	run("rm tmpcrontab")
+
+	# 4. Test app - Write test recipients and cc to config and change to production recipients and cc if test passes.
+	# Write test recipients and cc to config
+	run("echo 'recipients=\"%s\"' >> fabrice/reports/reports.cfg.sh" % test_recipients)
+	run("echo 'cc=\"%s\"' >> fabrice/reports/reports.cfg.sh" % test_cc)
+
+	# Test-run scripts
+	run("sh fabrice/reports/data_offer_rental.sh")
+	run("sh fabrice/reports/invoice.sh")
+
+	# Remove test recipients and cc from config
+	run("grep -v '%s' fabrice/reports/reports.cfg.sh | grep -v '%s' >> reports.cfg.sh.backup" % (test_recipients, test_cc))
+	run("cp reports.cfg.sh.backup fabrice/reports/reports.cfg.sh")
+	run("rm reports.cfg.sh.backup")
+
+	# Write live recipients and cc to config
+	run("echo 'recipients=\"%s\"' >> fabrice/reports/reports.cfg.sh" % live_recipients)
+	run("echo 'cc=\"%s\"' >> fabrice/reports/reports.cfg.sh" % live_cc)
     finally:
-	# Local
-	# =====
 	local("rm -rf fabrice*")
 
 @roles('concierge', 'lnp', 'middleware')
