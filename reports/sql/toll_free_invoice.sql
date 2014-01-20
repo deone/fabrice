a46ce52b15a488aeb09f39c425f09fc4453389e8
Oladayo Osikoya <alwaysdeone@gmail.com>
2014-01-20 16:49:20 +0100

    Oops! Forgot to add the SQL.

---
+++ reports/sql/toll_free_invoice.sql
@@ -0,0 +1,14 @@
+set linesize 1000
+set colsep ,
+set pagesize 0
+set verify off
+
+prompt MSISDN, Invoice
+
+SELECT service_info_v, (invoice_amt_n + tax_amt_n) / 100 
+FROM cb_account_service_list a, cb_sub_invoice b
+WHERE service_info_v LIKE '540118%'
+AND b.account_link_code_n = a.account_link_code_n
+AND bill_cycl_full_code_n = &1;
+
+EXIT;
