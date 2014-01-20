8b0fe0155407888fed62c5d7d7e1d9c8a00cbf3d
Oladayo Osikoya <alwaysdeone@gmail.com>
2014-01-20 16:55:00 +0100

    Add offer rental report.

--- reports/offer_rental.sh
+++ reports/offer_rental.sh
@@ -24,21 +24,7 @@ echo "Mobile Number, Rental Amount - Offer" > $sql_results
 
 # Fetch data
 sqlplus -s $conn_string >> $sql_results << EOF
-set linesize 1000
-set colsep ,
-set pagesize 0
 
-SELECT '233'||b.SERVICE_INFO_V, SUM(a.TRANS_AMT_N)/100 
-FROM cb_invoice_details a, cb_account_Service_list b
-WHERE bill_cycle_full_Code_n=101000001${year}${month_number}0
-AND a.serv_acc_link_code_n=b.account_link_code_n
-AND (article_code_V,scheme_Ref_code_n) IN (
-SELECT article_code_v, cs.scheme_ref_code_n
-FROM cb_subscription_articles cs, cb_offers cp WHERE cs.scheme_ref_code_n=cp.scheme_Ref_code_n)
-AND trans_amt_n >0
-GROUP BY '233'||b.SERVICE_INFO_V;
-
-EXIT;
 EOF
 
 # Compose email
