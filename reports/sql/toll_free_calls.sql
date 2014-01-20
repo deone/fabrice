59a22de9e2c3f86828c0c104a05fc498431793b0
Oladayo Osikoya <alwaysdeone@gmail.com>
2014-01-20 17:13:52 +0100

    Add toll free calls report.

---
+++ reports/sql/toll_free_calls.sql
@@ -0,0 +1,14 @@
+set linesize 1000
+set colsep ,
+set pagesize 0
+set verify off
+
+prompt Called Number, Calling Number, Date, Duration, Charge
+
+SELECT mobile_number_v, called_calling_number_v, 
+call_date_time_dt, duration_n, charge_amount_n / 1000 
+FROM cb_upload_all_cdrs
+WHERE mobile_number_v LIKE '540118%'
+AND bill_cycl_full_code_n = &1;
+
+EXIT;
