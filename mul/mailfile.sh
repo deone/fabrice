170c4878368ef16e2500281c8c61791b87c2cf67
Oladayo Osikoya <alwaysdeone@gmail.com>
2014-02-14 10:55:26 +0100

    Send, and delete, CL file according to plist.

---
+++ mul/mailfile.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+date
+email="/Users/deone/.virtualenvs/fabrice/fabrice/mul/out/email.txt"
+
+cat << EOF > $email
+Hello,
+
+Please modify credit limit for the attached numbers.
+
+Regards,
+Ramo :-)
+EOF
+
+clfile="/Users/deone/.virtualenvs/fabrice/fabrice/mul/out/cl.txt"
+recipients="eeswar.jagadisan@tecnotree.com"
+cc="osikoya.oladayo@tecnotree.com;chandra.mohan@tecnotree.com"
+
+if [ -f $clfile ]; then
+    /opt/boxen/homebrew/bin/mutt -s "Modify Credit Limit" -c $cc -a $clfile -- $recipients < $email
+    rm $clfile
+fi
