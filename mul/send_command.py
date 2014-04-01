6059d1b00b863cbb61d8fcb6edcdc6dbd38f5703
Oladayo Osikoya <alwaysdeone@gmail.com>
2014-04-01 10:13:18 +0100

    Make indentation 2 spaces. Remove emails from provisioning rejection report.

--- mul/send_command.py
+++ mul/send_command.py
@@ -28,19 +28,19 @@ from util import *
 
 
 def send_request(request):
-    """ Send `request` to url (already a part of `request`) and get response """
-    response = urllib2.urlopen(request)
-    return BeautifulSoup(response).resultmessage.string
+  """ Send `request` to url (already a part of `request`) and get response """
+  response = urllib2.urlopen(request)
+  return BeautifulSoup(response).resultmessage.string
 
 def get_msisdn_old_mul(command):
-    """ If we can't find mul value in `command`, write msisdn to file and print message on console. Else, return msisdn and mul value """
-    parts = command.split(',')
-    msisdn = parts[1].split(':')[0]
-    old_mul = parts[6]
-    if old_mul:
-	return [msisdn, old_mul]
-    write_to_file(strip_country_code(msisdn), cl_file)
-    print "No MUL value for %s. MSISDN written to file." % strip_country_code(msisdn)
+  """ If we can't find mul value in `command`, write msisdn to file and print message on console. Else, return msisdn and mul value """
+  parts = command.split(',')
+  msisdn = parts[1].split(':')[0]
+  old_mul = parts[6]
+  if old_mul:
+    return [msisdn, old_mul]
+  write_to_file(strip_country_code(msisdn), cl_file)
+  print "No MUL value for %s. MSISDN written to file." % strip_country_code(msisdn)
 
 def compute_new_mul(old_mul, usage_counter):
   """ Deduct `usage_counter` from `old_mul`. Return 0 if result is negative """
@@ -58,60 +58,60 @@ def get_usage_details(msisdn):
 
   soup = BeautifulSoup(response)
   result = {
-      'counter': soup.usagecountermonetaryvalue.string,
-      'threshold': soup.usagethresholdmonetaryvalue.string
+    'counter': soup.usagecountermonetaryvalue.string,
+    'threshold': soup.usagethresholdmonetaryvalue.string
   }
 
   return result
 
 def main(cmd_file, deduct_counter=False):
-    try:
-	open_file = open(cmd_file, 'r')
-    except Exception:
-	sys.exit("Exiting...Commands file not found")
-    else:
-	for line in open_file:
-	    try:
-		try:
-		    msisdn, old_mul = get_msisdn_old_mul(line)
-		except TypeError:
-		    continue
+  try:
+    open_file = open(cmd_file, 'r')
+  except Exception:
+    sys.exit("Exiting...Commands file not found")
+  else:
+    for line in open_file:
+      try:
+        try:
+          msisdn, old_mul = get_msisdn_old_mul(line)
+        except TypeError:
+          continue
 
-		if deduct_counter:
-		    usage_details = get_usage_details(msisdn)
-		    usage_counter = usage_details['counter']
-		    new_mul = compute_new_mul(old_mul, usage_counter)
-		    mul_command = build_mul_command(line, new_mul)
-		else:
-		    mul_command = line[:-1]
+        if deduct_counter:
+          usage_details = get_usage_details(msisdn)
+          usage_counter = usage_details['counter']
+          new_mul = compute_new_mul(old_mul, usage_counter)
+          mul_command = build_mul_command(line, new_mul)
+        else:
+          mul_command = line[:-1]
 
-		request = build_request(mul_command)
-		response = send_request(request)
+        request = build_request(mul_command)
+        response = send_request(request)
 
-		debug_info = (mul_command, response)
+        debug_info = (mul_command, response)
 
-		result = is_valid(response)
+        result = is_valid(response)
 
-		if result is True:
-		    write_to_file(str(debug_info), success)
+        if result is True:
+          write_to_file(str(debug_info), success)
 
-		    record = "'" + strip_country_code(msisdn) + "',"
-		    write_to_file(record, processed)
-		else:
-		    write_to_file(str(debug_info) + " " + error_codes[result], errors)
+          record = "'" + strip_country_code(msisdn) + "',"
+          write_to_file(record, processed)
+        else:
+          write_to_file(str(debug_info) + " " + error_codes[result], errors)
 
-	    except:
-		print_exc()
-		write_to_file(line, errors)
-		continue
-	os.remove(cmd_file)
+      except:
+        print_exc()
+        write_to_file(line, errors)
+        continue
+    os.remove(cmd_file)
 
 
 if __name__ == "__main__":
-    if len(sys.argv) == 1:
-	main(commands)
+  if len(sys.argv) == 1:
+    main(commands)
+  else:
+    if sys.argv[1] == '--deduct':
+      main(commands, deduct_counter=True)
     else:
-	if sys.argv[1] == '--deduct':
-	    main(commands, deduct_counter=True)
-	else:
-	    print('Unrecognized option. Please use --deduct')
+      print('Unrecognized option. Please use --deduct')
