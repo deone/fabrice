9b8079ebbd575e6b6f7a186d4bb777c374c0366e
Oladayo Osikoya <alwaysdeone@gmail.com>
2014-06-12 10:43:14 +0100

    Remove yesterday obsolete variable Add cs5 bulk file functionality.

--- checklists/bulk.sh
+++ checklists/bulk.sh
@@ -9,8 +9,6 @@ sql_file_name=`echo $@ | rev | cut -d '/' -f 1  | rev | cut -d '.' -f 1`
 
 results="${results_dir}/Bulk_${sql_file_name}_${date_string}.csv"
 
-yesterday=`date -v -1d +"%d"`
-
 find ${results_dir} -name '*.csv' -mtime +1 -exec rm -f {} \;
 
 sqlplus -S $conn_string @${sql_dir}/${sql_file_name}.sql > $results
