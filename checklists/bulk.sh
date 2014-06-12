bf94a81de1ee9875ffb48b112f391a50195b1680
Oladayo Osikoya <alwaysdeone@gmail.com>
2014-06-12 10:22:24 +0100

    Move results_dir into cfg.

--- checklists/bulk.sh
+++ checklists/bulk.sh
@@ -7,7 +7,6 @@ sql_file_name=`echo $@ | rev | cut -d '/' -f 1  | rev | cut -d '.' -f 1`
 # Config
 . ${FABRICE_PATH}checklists/checklists.cfg.sh
 
-results_dir="/Users/deone/Documents/Work/bulk_files"
 results="${results_dir}/Bulk_${sql_file_name}_${date_string}.csv"
 
 yesterday=`date -v -1d +"%d"`
--- checklists/checklists.cfg.sh
+++ checklists/checklists.cfg.sh
@@ -4,3 +4,4 @@ conn_string="abillity_view/abillity_view@TTPROD"
 date_string=`date +'%d%m%Y'`
 
 sql_dir="${FABRICE_PATH}checklists/sql"
+results_dir="/Users/deone/Documents/Work/bulk_files"
