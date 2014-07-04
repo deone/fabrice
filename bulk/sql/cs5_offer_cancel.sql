45b4fcbf5f6a351e1ba855c73ebbdaaf38d2ffe6
Oladayo Osikoya <alwaysdeone@gmail.com>
2014-07-04 11:58:17 +0100

    Remove sysdate from cs5 offer sqls.

--- bulk/sql/cs5_offer_cancel.sql
+++ bulk/sql/cs5_offer_cancel.sql
@@ -2,6 +2,6 @@
 
 select distinct gsm.MOBL_NUM_VOICE_V||','||s.OFFER_CODE_V||',DELETE'  
 from TMP_CS5_AFFECTED_SERVICES s,gsm_service_mast gsm 
-where s.DATE_D>=trunc(sysdate)  and s.ACCOUNT_LINK_CODE_N=gsm.ACCOUNT_LINK_CODE_N;
+where s.ACCOUNT_LINK_CODE_N=gsm.ACCOUNT_LINK_CODE_N;
 
 EXIT;
