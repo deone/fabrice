c7dffb883419f67d4de2c6177b626e7ac7d69500
Oladayo Osikoya <alwaysdeone@gmail.com>
2014-08-13 16:26:36 +0100

    Remove obsolete cs5 directory.

--- cs5/config.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-conn_string="tt_mso/ttmso1@TTPROD"
-date_string=`date +'%d%m%Y'`
-
-sql_dir="${FABRICE_PATH}cs5/sql"
-results_dir="/Users/deone/Documents/Work/bulk_files/cs5"
--- cs5/sql/insert.sql
+++
@@ -1,7 +0,0 @@
-set pagesize 0
-
-select gsm.MOBL_NUM_VOICE_V||','||s.OFFER_CODE_V||',ADD VAS,,' 
-from TMP_CS5_AFFECTED_SERVICES s,gsm_service_mast gsm 
-where s.DATE_D>=trunc(sysdate) and s.ACCOUNT_LINK_CODE_N=gsm.ACCOUNT_LINK_CODE_N;
-
-EXIT;
