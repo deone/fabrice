62583a2d41e51915810307c5932ef1df14f4cb09
Oladayo Osikoya <alwaysdeone@gmail.com>
2014-08-15 13:18:51 +0100

    Add distinct keyword to offer addition SQL.

--- bulk/sql/cs5_offer_addition.sql
+++ bulk/sql/cs5_offer_addition.sql
@@ -1,6 +1,6 @@
 @@commands.sql
 
-select gsm.MOBL_NUM_VOICE_V||','||s.OFFER_CODE_V||',ADD VAS,,' 
+select distinct gsm.MOBL_NUM_VOICE_V||','||s.OFFER_CODE_V||',ADD VAS,,' 
 from TMP_CS5_AFFECTED_SERVICES s,gsm_service_mast gsm 
 where s.ACCOUNT_LINK_CODE_N=gsm.ACCOUNT_LINK_CODE_N;
 
