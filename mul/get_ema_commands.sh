0abba2eb062306f2fae4b973bb832f0f1b85a548
Oladayo Osikoya <alwaysdeone@gmail.com>
2014-08-28 13:15:49 +0100

    Remove FABRICE_PATH. Normalize tabs and paths.

--- mul/get_ema_commands.sh
+++ mul/get_ema_commands.sh
@@ -1,34 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
 # 1. Select numbers from db.
 # 2. Generate EMA commands by running them through the MUL mismatch script.
 # 3. Fire commands to EMA endpoint.
 # 4. Update numbers as fixed in db.
 
-# Set/Fetch env. vars on both dev and live
-# Dev
-if [[ "$OSTYPE" == "darwin13" ]]; then
-    export FABRICE_DEBUG="true"
-    export FABRICE_PATH="/Users/deone/.virtualenvs/fabrice/fabrice"
-    . ~/.profile
-# Live
-elif [[ "$OSTYPE" == "linux-gnu" ]]; then
-    export FABRICE_DEBUG="false"
-    export FABRICE_PATH="/home/pm_client/fabrice"
-    . ~/.bash_profile
-fi
-
 # Load config
 . ${FABRICE_PATH}/mul/mul.cfg.sh
 
 # Fetch numbers from either file or DB as source.
 if [[ -n "$@" ]]; then
-    if [[ "$@" == "--source=db" ]]; then
-	sqlplus -S $conn_string @${FABRICE_PATH}/mul/sql/numbers.sql > $numbers
-    else
-	echo "Invalid option. Please use --source=db"
-	exit
-    fi
+  if [[ "$@" == "--source=db" ]]; then
+    sqlplus -S $conn_string @${FABRICE_PATH}/mul/sql/numbers.sql > $numbers
+  else
+    echo "Invalid option. Please use --source=db"
+    exit
+  fi
 fi
 
 # Create CSV file 
@@ -38,8 +25,8 @@ sqlplus -S $conn_string @${FABRICE_PATH}/mul/sql/truncate.sql
 
 while read line
 do
-    value="${line:0:${#line}-1}"
-    sqlplus -S $conn_string @${FABRICE_PATH}/mul/sql/mul.sql $value
+  value="${line:0:${#line}-1}"
+  sqlplus -S $conn_string @${FABRICE_PATH}/mul/sql/mul.sql $value
 done < $csv
 
 echo "commit;" | sqlplus -S $conn_string
--- mul/mark_done.sh
+++ mul/mark_done.sh
@@ -1,30 +1,19 @@
-#!/bin/sh
-
-if [ "$OSTYPE" == "darwin13" ]; then
-    export FABRICE_DEBUG="true"
-    export FABRICE_PATH="/Users/deone/.virtualenvs/fabrice/fabrice/"
-    . ~/.profile
-# Live
-elif [ "$OSTYPE" == "linux-gnu" ]; then
-    export FABRICE_DEBUG="false"
-    export FABRICE_PATH="/home/pm_client/fabrice/"
-    . ~/.bash_profile
-fi
+#!/bin/bash
 
 # Load config
-. ${FABRICE_PATH}mul/mul.cfg.sh
+. $FABRICE_PATH/mul/mul.cfg.sh
 
 if [ -f $processed ]; then
-    while read line
-    do
-	value="${line:0:${#line}-1}"
-	sqlplus -S $conn_string @${FABRICE_PATH}mul/sql/update.sql $value
-    done < $processed
+  while read line
+  do
+    value="${line:0:${#line}-1}"
+    sqlplus -S $conn_string @${FABRICE_PATH}/mul/sql/update.sql $value
+  done < $processed
 
-    echo "commit;" | sqlplus -S $conn_string
-    rm $processed
+  echo "commit;" | sqlplus -S $conn_string
+  rm $processed
 else
-    echo "Exiting...Processed numbers file not found"
+  echo "Exiting...Processed numbers file not found"
 fi
 
 exit
