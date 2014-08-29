6074a0782cbcaa7553535f894a1e238a978a7d4a
Oladayo Osikoya <alwaysdeone@gmail.com>
2014-08-29 11:24:22 +0100

    Support files that don't need to be serialized.

--- bulk/messenger.sh
+++ bulk/messenger.sh
@@ -15,7 +15,12 @@ fi
 sqlplus -S $conn_string @${sql_dir}/${sql_file_name}.sql > $temp_file
 
 file_header=$(create_file_header $key $temp_file)
-file=$(serialize_file_name $results_root_dir/$dir $sql_file_name)
+
+if [ "$dir" != "checklists" ]; then
+  file=$(serialize_file_name $results_root_dir/$dir $sql_file_name)
+else
+  file="$results_root_dir/$dir/${sql_file_name}_$date_string.csv"
+fi
 
 echo $file_header > $file
 cat $temp_file >> $file
