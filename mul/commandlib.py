e386c72970342e4e3171f851f1d3c28579cbb683
Oladayo Osikoya <alwaysdeone@gmail.com>
2014-09-08 10:50:28 +0100

    More PEP 8.

--- mul/commandlib.py
+++ mul/commandlib.py
@@ -4,69 +4,69 @@ from bs4 import BeautifulSoup
 from settings import url
 
 def send_request(request):
-  """ Send `request` to url (already a part of `request`) and get response """
-  proxy = urllib2.ProxyHandler({'http': 'http://oosikoya:cba$321%@10.135.15.176:8080'})
-  auth = urllib2.HTTPBasicAuthHandler()
-  opener = urllib2.build_opener(proxy, auth, urllib2.HTTPHandler)
-  urllib2.install_opener(opener)
+    """ Send `request` to url (already a part of `request`) and get response """
+    proxy = urllib2.ProxyHandler({'http': 'http://oosikoya:cba$321%@10.135.15.176:8080'})
+    auth = urllib2.HTTPBasicAuthHandler()
+    opener = urllib2.build_opener(proxy, auth, urllib2.HTTPHandler)
+    urllib2.install_opener(opener)
 
-  response = urllib2.urlopen(request)
-  return BeautifulSoup(response).resultmessage.string
+    response = urllib2.urlopen(request)
+    return BeautifulSoup(response).resultmessage.string
 
 def build_simple_command(msisdn, cmd_type):
-  """ Build usage counter command with `msisdn` """
-  if cmd_type == "mul":
-    return "GET:AIRSUB:MSISDN,%s:MSISDNNAI,1:REQUESTEDOWNER,3;" % msisdn
-  else:
-    return "SET:GSMSUB:MSISDN,233%s:SERVICECLASS,71;" % msisdn
+    """ Build usage counter command with `msisdn` """
+    if cmd_type == "mul":
+        return "GET:AIRSUB:MSISDN,%s:MSISDNNAI,1:REQUESTEDOWNER,3;" % msisdn
+    else:
+        return "SET:GSMSUB:MSISDN,233%s:SERVICECLASS,71;" % msisdn
 
 def build_mul_command(old_command, value):
-  """ Build mul command with `old_command` and `value` """
-  parts = old_command.split(',')
-  return "%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s" % (
-      parts[0],
-      parts[1],
-      parts[2],
-      parts[3],
-      parts[4],
-      parts[5],
-      value,
-      parts[7],
-      parts[8],
-      parts[9],
-      parts[10],
-      parts[11],
-      parts[12],
-      parts[13],
-      parts[14],
-      parts[15],
-      parts[16],
-      parts[17],
-      parts[18][:-1]
-  )
+    """ Build mul command with `old_command` and `value` """
+    parts = old_command.split(',')
+    return "%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s" % (
+        parts[0],
+        parts[1],
+        parts[2],
+        parts[3],
+        parts[4],
+        parts[5],
+        value,
+        parts[7],
+        parts[8],
+        parts[9],
+        parts[10],
+        parts[11],
+        parts[12],
+        parts[13],
+        parts[14],
+        parts[15],
+        parts[16],
+        parts[17],
+        parts[18][:-1]
+    )
 
 def build_request(command):
-  """ Build a request with `command` """
-  xml = """
-  <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ema="http://schema.concierge.com/ema">
-      <soapenv:Header/>
-      <soapenv:Body>
-          <ema:processRequest>
-              <!--Optional:-->
-              <ema:EMA>
-                  <ema:EMARequest>
-                      <ema:Request>%s</ema:Request>
-                  </ema:EMARequest>
-                  <ema:EMAResponse>
-                      <!--Optional:-->
-                      <ema:resultcode>?</ema:resultcode>
-                      <!--Optional:-->
-                      <ema:resultmessage>?</ema:resultmessage>
-                  </ema:EMAResponse>
-              </ema:EMA>
-          </ema:processRequest>
-      </soapenv:Body>
-  </soapenv:Envelope>
-  """ % command
-  request = urllib2.Request(url, xml)
-  return request
+    """ Build a request with `command` """
+    xml = """
+    <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ema="http://schema.concierge.com/ema">
+        <soapenv:Header/>
+        <soapenv:Body>
+            <ema:processRequest>
+                <!--Optional:-->
+                <ema:EMA>
+                    <ema:EMARequest>
+                        <ema:Request>%s</ema:Request>
+                    </ema:EMARequest>
+                    <ema:EMAResponse>
+                        <!--Optional:-->
+                        <ema:resultcode>?</ema:resultcode>
+                        <!--Optional:-->
+                        <ema:resultmessage>?</ema:resultmessage>
+                    </ema:EMAResponse>
+                </ema:EMA>
+            </ema:processRequest>
+        </soapenv:Body>
+    </soapenv:Envelope>
+    """ % command
+    request = urllib2.Request(url, xml)
+    return request
--- mul/settings.py
+++ mul/settings.py
@@ -3,11 +3,11 @@ import platform
 url = "http://10.139.41.58:6004/EMA/EMA_PROXY"
 
 if platform.system() == "Darwin":
-  fabrice_mul_path = "/Users/deone/.virtualenvs/fabrice/fabrice/mul"
+    fabrice_mul_path = "/Users/deone/.virtualenvs/fabrice/fabrice/mul"
 elif platform.system() == "Linux":
-  fabrice_mul_path = "/home/pm_client/fabrice/mul"
+    fabrice_mul_path = "/home/pm_client/fabrice/mul"
 elif platform.system() == "Windows":
-  fabrice_mul_path = "C:/fabrice/mul"
+    fabrice_mul_path = "C:/fabrice/mul"
     
 commands = "%s/out/commands.out" % fabrice_mul_path
 cl_file = "%s/out/cl.txt" % fabrice_mul_path
@@ -16,8 +16,8 @@ success = "%s/logs/success.log" % fabrice_mul_path
 errors = "%s/logs/errors.log" % fabrice_mul_path
 
 error_codes = {
-  '17204': 'Temporarily Blocked',
-  '17105': 'Internal Server Error',
-  '17103': 'Data out of bounds',
-  '17232': 'Subscriber not found'
+    '17204': 'Temporarily Blocked',
+    '17105': 'Internal Server Error',
+    '17103': 'Data out of bounds',
+    '17232': 'Subscriber not found'
 }
